de4846b7c7545cb355519e12dcb50a7ff77fc24b
saleem.jibril <saleem.jibril@netzence.com>
2024-05-18 01:33:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
